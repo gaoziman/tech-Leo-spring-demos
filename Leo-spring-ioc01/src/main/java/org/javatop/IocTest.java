f4371894434dd29fd620ea873e6f911352657024
gaoziman <2942894660@qq.com>
2023-11-27 23:49:31 +0800

    获取bean的几种方式

--- Leo-spring-ioc01/src/main/java/org/javatop/IocTest.java
+++ Leo-spring-ioc01/src/main/java/org/javatop/IocTest.java
@@ -22,8 +22,18 @@ public class IocTest {
         // 1.创建Spring容器 去加载beans.xml文件
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
         // 2.获取bean对象
-        Person person = (Person) ctx.getBean("person");
-        System.out.println("person = " + person);
+
+        // 根据名字获取对象
+        Person person1 = (Person) ctx.getBean("person");
+        System.out.println("person1 = " + person1);
+        // 根据类获取对象 通过class
+        Person person2 = ctx.getBean("person", Person.class);
+        System.out.println("person2 = " + person2);
+
+        //根据类型获取对象
+        Person person3 = ctx.getBean(Person.class);
+        System.out.println("person3 = " + person3);
+
 
     }
 }
